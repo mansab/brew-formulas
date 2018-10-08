ee6f72ad0d42360a78513285744e1b644af8df45
Mansab Uppal <muppal@thoughtworks.com>
2018-10-08 16:09:51 +0200

    mu update shasum hash

--- otr-base-images.rb
+++ otr-base-images.rb
@@ -2,7 +2,7 @@ class OtrBaseImages < Formula
   desc "Builds all the base images required for OTR services"
   homepage "https://gitea.app.oscp.mercedes-benz.com/mbd-otr/brew-formulas"
   url "https://gitea.app.oscp.mercedes-benz.com/mbd-otr/alpine-base-image/raw/branch/master/scripts/otr-base-images.tar.gz"
-  sha256 "dafaad7bc6e918650bf9d7ea735ee49ec8b2328b487bd3d3e6b891931be0dccb"
+  sha256 "48b6264a534f0e3483402ae2822936f4c04b29d08f88555449cdf7f5596bedb0"
   version "0.0.1"
 
   depends_on "git"
